252a453f15b80bc3a331ee1e4d31c7528ebca13a
이정현 <fnsk1256@naver.com>
2025-03-28 13:24:59 +0900

    feat: 헤더 적용

--- front-end/src/app/profile/page.tsx
+++ front-end/src/app/profile/page.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/common/Header';
 
 export default function Page() {
   return (
-    <div className="container">
+    <div className="container bg-gray-50">
       <Header hasBackButton title="내 정보" hasBorder={false} />내 정보 페이지
     </div>
   );
