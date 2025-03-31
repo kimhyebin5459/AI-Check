86d750c5179936688aac798cb6a119c0a5d8df98
kimhyebin <bin5459@naer.com>
2025-03-31 14:24:32 +0900

    feat: 캘린더 보기, 조회, 날짜 선택 등 구현

--- front-end/src/mocks/handlers/index.ts
+++ front-end/src/mocks/handlers/index.ts
@@ -1,4 +1,5 @@
 import { dutchPayHandlers } from './dutchPay';
 import { transactionHandlers } from './transaction';
+import calendarHandlers from './calendar';
 
-export const handlers = [...dutchPayHandlers, ...transactionHandlers];
+export const handlers = [...dutchPayHandlers, ...transactionHandlers, ...calendarHandlers];
---
+++ front-end/src/types/calendar.ts
@@ -0,0 +1,22 @@
+// types/calendar.ts
+
+export interface CalendarDataItem {
+  date: string;
+  sum: number;
+}
+
+export interface CalendarResponse {
+  calendar: CalendarDataItem[];
+}
+
+export interface CalendarDay {
+  day: number | null;
+  active: boolean;
+  date: Date | null;
+  sum: number | null;
+}
+
+export interface ErrorResponse {
+  code: string;
+  message: string;
+}
