35eb56711eaccbd4ab430f8cd9f12e42a698fd61
이정현 <fnsk1256@naver.com>
2025-04-03 02:45:10 +0900

    design: UI 수정

--- front-end/src/app/mother-ai/list/page.tsx
+++ front-end/src/app/mother-ai/list/page.tsx
@@ -5,16 +5,18 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <div className="bg-gradation1 container h-full w-full">
-      <Header title="엄마 AI" hasBackButton />
+    <div className="bg-gradation1 container">
+      <Header title="엄마 AI" hasBackButton hasBorder={false} />
       <main className="flex h-full w-full flex-col justify-end">
-        <div className="flex items-end">
-          <h1 className="text-mdl pr-3 pb-7 pl-5 font-bold">
+        <div className="flex items-end justify-between px-5">
+          <h1 className="pb-7 text-2xl font-bold whitespace-nowrap">
             자녀마다
             <br />
             AI의 설정을 변경해요
           </h1>
-          <Image src="/images/cuteRobotWithHeart.png" alt="로봇 이미지" width={114} height={230}></Image>
+          <div className="flex w-full justify-center">
+            <Image src="/images/cuteRobotWithHeart.png" alt="로봇 이미지" width={90} height={177}></Image>
+          </div>
         </div>
 
         <ChildList />
