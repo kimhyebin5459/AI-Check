a9b20480f5df1a6a1c55e2857641ac8b70718646
이정현 <fnsk1256@naver.com>
2025-04-06 00:06:17 +0900

    feat: 내 정보 페이지 api 연결

---
+++ front-end/src/components/profile/ProfileSection.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import ProfileImage from '@/components/common/ProfileImage';
+import useGetUserInfo from '@/hooks/query/useGetUserInfo';
+import Link from 'next/link';
+
+export default function Page() {
+  const { data: user } = useGetUserInfo();
+
+  return (
+    <>
+      <div className="space-y-3 pt-10 text-center">
+        <ProfileImage image={user?.image} size="xl" />
+        <p className="text-2xl font-bold text-gray-800">{user?.name}</p>
+      </div>
+      <div className="w-full space-y-7 rounded-xl bg-white px-6 py-7">
+        <div className="flex w-full items-center">
+          <p className="min-w-24 text-xl font-semibold text-gray-600">이름</p>
+          <div className="flex w-full justify-between">
+            <p className="font-light">{user?.name}</p>
+            <Link href={'/profile/edit'} className="-m-2 p-2 font-light text-gray-400">
+              변경
+            </Link>
+          </div>
+        </div>
+        <div className="flex w-full items-center">
+          <p className="min-w-24 text-xl font-semibold text-gray-600">생년월일</p>
+          <p className="font-light">{user?.birth.replaceAll('-', '.')}</p>
+        </div>
+        <div className="flex w-full items-start">
+          <p className="min-w-24 text-xl font-semibold text-gray-600">연동 계좌</p>
+          <div>
+            <p className="font-light">{user?.account.no}</p>
+            <p className="-mt-1.5 text-sm font-light text-gray-400">{user?.account.name}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
