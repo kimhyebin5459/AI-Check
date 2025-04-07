cd8d6241afdb1640f90ed3b914be410bf5e9b1bb
kimhyebin <bin5459@naer.com>
2025-04-07 22:05:32 +0900

    feat: 알림 읽음 처리 구현

--- front-end/src/app/(with-footer)/notification/page.tsx
+++ front-end/src/app/(with-footer)/notification/page.tsx
@@ -4,9 +4,25 @@ import Header from '@/components/common/Header';
 import NotificationItem from '@/components/notification/NotificationItem';
 import Spinner from '@/components/common/Spinner';
 import useNotification from '@/hooks/query/useNotification';
+import { patchNotification } from '@/apis/notification';
+import { useQueryClient } from '@tanstack/react-query';
+import { Notification } from '@/types/notification';
 
 export default function Page() {
   const { data: notificationList, isLoading } = useNotification();
+  const queryClient = useQueryClient();
+
+  const handleReadChange = async (alarmId: number) => {
+    try {
+      await patchNotification(alarmId);
+
+      queryClient.setQueryData(['notifications'], (oldData: Notification[]) =>
+        oldData?.map((noti) => (noti.alarmId === alarmId ? { ...noti, isRead: true } : noti))
+      );
+    } catch (error) {
+      console.error('알림 읽음 처리 실패:', error);
+    }
+  };
 
   return (
     <div className="container">
@@ -18,7 +34,9 @@ export default function Page() {
       ) : (
         <div className="h-full w-full space-y-1 overflow-y-auto">
           {notificationList && notificationList.length > 0 ? (
-            notificationList.map((noti) => <NotificationItem key={noti.alarmId} {...noti} />)
+            notificationList.map((noti) => (
+              <NotificationItem key={noti.alarmId} {...noti} onReadChange={handleReadChange} />
+            ))
           ) : (
             <div className="flex h-full w-full items-center justify-center p-8 text-gray-500">아직 알림이 없습니다</div>
           )}
