57d276c770352fb60e73079eeccc37197b4a1851
kimhyebin <bin5459@naer.com>
2025-04-07 15:50:31 +0900

    fix: env 수정

--- front-end/src/app/(with-footer)/notification/page.tsx
+++ front-end/src/app/(with-footer)/notification/page.tsx
@@ -1,16 +1,25 @@
+'use client';
+
 import Header from '@/components/common/Header';
 import NotificationItem from '@/components/notification/NotificationItem';
-import { notificationList } from '@/mocks/fixtures/notification';
+import Spinner from '@/components/common/Spinner';
+import useNotification from '@/hooks/query/useNotification';
 
 export default function Page() {
+  const { data: notificationList, isLoading } = useNotification();
+
   return (
     <div className="container">
       <Header title="알림" hasBorder={false} />
-      <div className="w-full space-y-1 overflow-y-auto">
-        {notificationList.map((noti) => (
-          <NotificationItem key={noti.alarmId} {...noti} />
-        ))}
-      </div>
+      {isLoading ? (
+        <div className="flex justify-center p-4">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="w-full space-y-1 overflow-y-auto">
+          {notificationList?.map((noti) => <NotificationItem key={noti.alarmId} {...noti} />)}
+        </div>
+      )}
     </div>
   );
 }
