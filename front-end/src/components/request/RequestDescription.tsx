ed4c5ae472b0a743f584e779b0e4c997e95c9ec0
kimhyebin <bin5459@naer.com>
2025-04-10 10:44:52 +0900

    fix: 말풍선 크기 벗어남 문제 해결

--- front-end/src/components/request/RequestDescription.tsx
+++ front-end/src/components/request/RequestDescription.tsx
@@ -1,5 +1,4 @@
-import { SpeechBubble } from '@/public/icons';
-import Image from 'next/image';
+import React from 'react';
 
 interface Props {
   text: string;
@@ -7,11 +6,37 @@ interface Props {
 
 export default function RequestDescription({ text }: Props) {
   return (
-    <div className="relative inline-block animate-bounce">
-      <Image src={SpeechBubble} alt="speech icon" />
-      <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform pb-4 text-center text-base font-light whitespace-nowrap">
-        {text}
-      </p>
+    <div className="animate-subtle-bounce relative inline-block">
+      {/* 말풍선 본체 */}
+      <div className="relative rounded-xl bg-white px-6 py-3 shadow-md">
+        <p className="text-center text-base font-light break-words">{text}</p>
+      </div>
+
+      {/* 말풍선 꼬리 (SVG) */}
+      <div className="absolute -bottom-2 left-1/2 -translate-x-1/2">
+        <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <filter id="shadow" x="-20%" y="-20%" width="140%" height="140%">
+            <feDropShadow dx="0" dy="1" stdDeviation="1" floodOpacity="0.2" />
+          </filter>
+          <path d="M0 0L10 10L20 0H0Z" fill="white" filter="url(#shadow)" />
+        </svg>
+      </div>
+
+      {/* 커스텀 애니메이션을 위한 스타일 */}
+      <style jsx>{`
+        @keyframes subtleBounce {
+          0%,
+          100% {
+            transform: translateY(0);
+          }
+          50% {
+            transform: translateY(-5px);
+          }
+        }
+        .animate-subtle-bounce {
+          animation: subtleBounce 2s ease-in-out infinite;
+        }
+      `}</style>
     </div>
   );
 }
